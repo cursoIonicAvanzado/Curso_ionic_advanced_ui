0ccc73e1137de5504d03ed6ad1ad68970ae780de
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-07-02 16:56:00 -0500

    Se implementó el ActionSheet

--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
-# Promises example
+# Advanced UI
 
-Este proyecto muestra el uso de promises de Angularjs en un proyecto de Ionic.
+Este proyecto muestra el uso de algunos componentes de UI de Ionic avanzados.
 
 ## Instrucciones
 
 ```
-git clone https://github.com/cursoIonicAvanzado/Curso_ionic_promises.git
-cd Curso_ionic_promises
+git clone https://github.com/cursoIonicAvanzado/Curso_ionic_advanced_ui.git
+cd Curso_ionic_advanced_ui
 ionic serve
 ```
--- www/js/ActionSheetCtrl.js
+++ www/js/ActionSheetCtrl.js
@@ -1,4 +1,25 @@
 angular.module('starter.actionsheet', [])
-    .controller('ActionSheetCtrl', function ($scope) {
-
+    .controller('ActionSheetCtrl', function ($scope, $ionicActionSheet) {
+        $scope.showActionsheet = function () {
+            $ionicActionSheet.show({
+                titleText: 'ActionSheet Example',
+                buttons: [
+                    {text: '<i class="icon ion-share"></i> Share'},
+                    {text: '<i class="icon ion-arrow-move"></i> Move'},
+                ],
+                destructiveText: 'Delete',
+                cancelText: 'Cancel',
+                cancel: function () {
+                    console.log('CANCELLED');
+                },
+                buttonClicked: function (index) {
+                    console.log('BUTTON CLICKED', index);
+                    return true;
+                },
+                destructiveButtonClicked: function () {
+                    console.log('DESTRUCT');
+                    return true;
+                }
+            });
+        };
     });
